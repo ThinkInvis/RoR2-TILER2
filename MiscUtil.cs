12c1f14f68531f97f51f56b73dfd6fd5f399e72f
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-08-25 17:27:43 -0500

    Document MiscUtil

--- MiscUtil.cs
+++ MiscUtil.cs
@@ -13,6 +13,9 @@ using System.Collections.ObjectModel;
 using RoR2.Navigation;
 
 namespace TILER2 {
+    /// <summary>
+    /// Contains miscellaneous utilities for working with a myriad of RoR2/R2API features, as well as some other math/reflection standalones.
+    /// </summary>
     public static class MiscUtil {
         private static Type nodeRefType;
         private static Type nodeRefTypeArr;
@@ -39,6 +42,13 @@ namespace TILER2 {
             }
         }*/
 
+        /// <summary>
+        /// Wraps a float within the bounds of two other floats.
+        /// </summary>
+        /// <param name="x">The number to perform a wrap operation on.</param>
+        /// <param name="min">The lower bound of the wrap operation.</param>
+        /// <param name="max">The upper bound of the wrap operation.</param>
+        /// <returns>The result of the wrap operation of x within min, max.</returns>
         public static float Wrap(float x, float min, float max) {
             if(x < min)
                 return max - (min - x) % (max - min);
@@ -46,44 +56,87 @@ namespace TILER2 {
                 return min + (x - min) % (max - min);
         }
 
+        /// <summary>
+        /// Uses reflection to subscribe an event handler to an EventInfo.
+        /// </summary>
+        /// <param name="evt">The EventInfo to subscribe to.</param>
+        /// <param name="o">The object instance to apply this subscription to.</param>
+        /// <param name="lam">The method to subscribe with.</param>
         public static void ReflAddEventHandler(this EventInfo evt, object o, Action<object, EventArgs> lam) {
             var pArr = evt.EventHandlerType.GetMethod("Invoke").GetParameters().Select(p=>Expression.Parameter(p.ParameterType)).ToArray();
             var h = Expression.Lambda(evt.EventHandlerType, Expression.Call(Expression.Constant(lam),lam.GetType().GetMethod("Invoke"),pArr[0],pArr[1]),pArr).Compile();
             evt.AddEventHandler(o, h);
         }
 
-        //Collection of unique class instances which all inherit the same type
+        /// <summary>
+        /// A collection of unique class instances which all inherit the same base type.
+        /// </summary>
+        /// <typeparam name="T">The type to enforce inheritance from for the contents of the FilingDictionary.</typeparam>
         public class FilingDictionary<T> : IEnumerable<T> {
             private readonly Dictionary<Type, T> _dict = new Dictionary<Type, T>();
 
+            /// <summary>
+            /// Gets the number of instances contained in the FilingDictionary.
+            /// </summary>
             public int Count => _dict.Count;
 
+            /// <summary>
+            /// Add an object inheriting from <typeparamref name="T"/> to the FilingDictionary. Throws an ArgumentException if an element of the object's type is already contained.
+            /// </summary>
+            /// <param name="inst">The object to add.</param>
             public void Add(T inst) {
                 _dict.Add(inst.GetType(), inst);
             }
 
+            /// <summary>
+            /// Add an object of type <typeparamref name="subT"/> to the FilingDictionary. Throws an ArgumentException if an element of the object's type is already contained.
+            /// </summary>
+            /// <typeparam name="subT">The type of the object to add.</typeparam>
+            /// <param name="inst">The object to add.</param>
             public void Add<subT>(subT inst) where subT : T {
                 _dict.Add(typeof(subT), inst);
             }
 
+            /// <summary>
+            /// Add an object of type <typeparamref name="subT"/> to the FilingDictionary, or replace one of the same type if it already exists.
+            /// </summary>
+            /// <typeparam name="subT">The type of the object to insert.</typeparam>
+            /// <param name="inst">The object to insert.</param>
             public void Set<subT>(subT inst) where subT : T {
                 _dict[typeof(subT)] = inst;
             }
 
+            /// <summary>
+            /// Attempts to get an object of type <typeparamref name="subT"/> from the FilingDictionary. 
+            /// </summary>
+            /// <typeparam name="subT">The type of the object to retrieve.</typeparam>
+            /// <returns>The unique object matching type <typeparamref name="subT"/> within this FilingDictionary if such an object exists; null otherwise.</returns>
             public subT Get<subT>() where subT : T {
                 return (subT)_dict[typeof(subT)];
             }
-
+            
+            /// <summary>
+            /// Removes the given object from the FilingDictionary.
+            /// </summary>
+            /// <param name="T">The object to remove.</typeparam>
             public void Remove(T inst) {
                 _dict.Remove(inst.GetType());
             }
 
+            /// <summary>
+            /// Removes all objects from the FilingDictionary which match a predicate.
+            /// </summary>
+            /// <param name="predicate">The predicate to filter removals by.</param>
             public void RemoveWhere(Func<T, bool> predicate) {
                 foreach (var key in _dict.Values.Where(predicate).ToList()) {
                     _dict.Remove(key.GetType());
                 }
             }
 
+            /// <summary>
+            /// Returns an enumerator that iterates through the FilingDictionary's contained objects.
+            /// </summary>
+            /// <returns>An enumerator that iterates through the FilingDictionary's contained objects.</returns>
             public IEnumerator<T> GetEnumerator() {
                 return _dict.Values.GetEnumerator();
             }
@@ -93,15 +146,36 @@ namespace TILER2 {
             }
         }
 
+        /// <summary>
+        /// Formats a float as a fixed-precision percentage string.
+        /// </summary>
+        /// <param name="tgt">The number to format.</param>
+        /// <param name="prec">The fixed decimal precision of the output.</param>
+        /// <param name="mult">The amount to multiply the number by before formatting.</param>
+        /// <returns></returns>
         public static string Pct(float tgt, uint prec = 0, float mult = 100f) {
             return (tgt*mult).ToString("N" + prec) + "%";
         }
+
+        /// <summary>
+        /// Retrieves the plural form to use in references to a certain number, using a fixed precision to help with epsilon cases.
+        /// </summary>
+        /// <param name="tgt">The number to analyze.</param>
+        /// <param name="prec">The fixed decimal precision to analyze at.</param>
+        /// <returns>"" if tgt is approximately -1 or 1; "s" otherwise</returns>
         public static string NPlur(float tgt, uint prec = 0) {
             if(prec == 0)
                 return (tgt == 1 || tgt == -1) ? "" : "s";
             else
                 return (Math.Abs(Math.Abs(tgt)-1) < Math.Pow(10,-prec)) ? "" : "s";
         }
+
+        /// <summary>
+        /// Calculates the projected increase in RoR2.Run.instance.difficultyCoefficient after a certain amount of passed time and completed stages.
+        /// </summary>
+        /// <param name="time">The amount of passed time to simulate.</param>
+        /// <param name="stages">The number of completed stages to simulate.</param>
+        /// <returns>A float representing the increase in difficulty coefficient that the given parameters would cause.</returns>
         public static float GetDifficultyCoeffIncreaseAfter(float time, int stages) {
 			DifficultyDef difficultyDef = DifficultyCatalog.GetDifficultyDef(Run.instance.selectedDifficulty);
 			float num2 = Mathf.Floor((Run.instance.GetRunStopwatch() + time) * 0.0166666675f);
@@ -110,10 +184,23 @@ namespace TILER2 {
 			float num9 = Mathf.Pow(1.15f, (float)Run.instance.stageClearCount + (float)stages);
 			return (num4 + num7 * num2) * num9 - Run.instance.difficultyCoefficient;
         }
+
+        /// <summary>
+        /// Returns a list of all CharacterMasters which have living CharacterBodies.
+        /// </summary>
+        /// <param name="playersOnly">If true, only CharacterMasters which are linked to a PlayerCharacterMasterController will be matched.</param>
+        /// <returns>A list of all CharacterMasters which have living CharacterBodies (and PlayerCharacterMasterControllers, if playersOnly is true).</returns>
         public static List<CharacterMaster> AliveList(bool playersOnly = false) {
             if(playersOnly) return PlayerCharacterMasterController.instances.Where(x=>x.isConnected && x.master && x.master.hasBody && x.master.GetBody().healthComponent.alive).Select(x=>x.master).ToList();
             else return CharacterMaster.readOnlyInstancesList.Where(x=>x.hasBody && x.GetBody().healthComponent.alive).ToList();
         }
+
+        /// <summary>
+        /// Spawn an item of the given tier at the position of the given CharacterBody.
+        /// </summary>
+        /// <param name="src">The body to spawn an item from.</param>
+        /// <param name="tier">The tier of item to spawn. Must be within 0 and 5, inclusive (Tier 1, Tier 2, Tier 3, Lunar, Equipment, Lunar Equipment).</param>
+        /// <param name="rng">An instance of Xoroshiro128Plus to use for random item selection.</param>
         public static void SpawnItemFromBody(CharacterBody src, int tier, Xoroshiro128Plus rng) {
             List<PickupIndex> spawnList;
             switch(tier) {
@@ -141,6 +228,13 @@ namespace TILER2 {
             PickupDropletController.CreatePickupDroplet(spawnList[rng.RangeInt(0,spawnList.Count)], src.transform.position, new Vector3(UnityEngine.Random.Range(-5.0f, 5.0f), 20f, UnityEngine.Random.Range(-5.0f, 5.0f)));
         }
 
+        /// <summary>
+        /// Removes a node from a NodeGraph. See the NodeOccupationInfo component.
+        /// </summary>
+        /// <param name="self">The DirectorCore to perform NodeGraph operations with.</param>
+        /// <param name="nodeGraph">The NodeGraph to remove nodes from.</param>
+        /// <param name="nodeIndex">The NodeIndex within the given NodeGraph to remove.</param>
+        /// <returns>True on successful removals; false otherwise.</returns>
         public static bool RemoveOccupiedNode(this DirectorCore self, NodeGraph nodeGraph, NodeGraph.NodeIndex nodeIndex) {
             var ocnf = self.GetType().GetFieldCached("occupiedNodes");
             Array ocn = (Array)ocnf.GetValue(self);
@@ -174,6 +268,12 @@ namespace TILER2 {
             return true;
         }
 
+        /// <summary>
+        /// Removes all NodeGraph nodes that a given GameObject occupies. See the NodeOccupationInfo component.
+        /// </summary>
+        /// <param name="self">The DirectorCore to perform NodeGraph operations with.</param>
+        /// <param name="obj">The GameObject to remove linked nodes with.</param>
+        /// <returns>True if no known nodes remain registered to the GameObject after execution; false otherwise.</returns>
         public static bool RemoveAllOccupiedNodes(this DirectorCore self, GameObject obj) {
             var cpt = obj.GetComponent<NodeOccupationInfo>();
             if(!cpt) return true;
@@ -181,7 +281,12 @@ namespace TILER2 {
             return cpt._indices.Count == 0;
         }
 
-        ///<summary>For use while destroying a GameObject and creating another one in its place (e.g. runtime replacement of a placeholder with an unknown prefab).</summary>
+        /// <summary>
+        /// Transfers NodeOccupationInfo from one object to another. For use while destroying a GameObject and creating another one in its place (e.g. runtime replacement of a placeholder with an unknown prefab).
+        /// </summary>
+        /// <param name="self">The DirectorCore to perform NodeGraph operations with.</param>
+        /// <param name="oldObj">The to-dispose object to retrieve NodeGraph info from.</param>
+        /// <param name="newObj">The new object to add NodeGraph info to.</param>
         public static void UpdateOccupiedNodesReference(this DirectorCore self, GameObject oldObj, GameObject newObj) {
             var oldcpt = oldObj.GetComponent<NodeOccupationInfo>();
             var newcpt = newObj.GetComponent<NodeOccupationInfo>();
@@ -229,8 +334,12 @@ namespace TILER2 {
         }
     }
 
+    /// <summary>
+    /// Contains information about which nodes in various NodeGraphs the attached GameObject occupies. Cannot track the OccupyNearbyNodes component.
+    /// </summary>
     public class NodeOccupationInfo : MonoBehaviour {
         internal readonly List<KeyValuePair<NodeGraph, NodeGraph.NodeIndex>> _indices;
+        /// <summary>A list of all node indices which this GameObject occupies.</summary>
         public readonly ReadOnlyCollection<KeyValuePair<NodeGraph, NodeGraph.NodeIndex>> indices;
         public NodeOccupationInfo() {
             _indices = new List<KeyValuePair<NodeGraph, NodeGraph.NodeIndex>>();
