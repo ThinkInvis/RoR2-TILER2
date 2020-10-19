aabff9eaee2c549e891e6719a91925e8dc094944
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-10-19 15:07:54 -0500

    Additional patches + additions for StatHooks

--- Modules/StatHooks.cs
+++ Modules/StatHooks.cs
@@ -24,11 +24,15 @@ namespace TILER2 {
             public float healthMultAdd = 0f;
             /// <summary>Added to base health. MAX_HEALTH ~ (BASE_HEALTH + baseHealthAdd) * (HEALTH_MULT + healthMultAdd).</summary>
             public float baseHealthAdd = 0f;
+            /// <summary>Added to base shield. MAX_SHIELD ~ BASE_SHIELD + baseShieldAdd.</summary>
+            public float baseShieldAdd = 0f;
             /// <summary>Added to the direct multiplier to base health regen. HEALTH_REGEN ~ (BASE_REGEN + baseRegenAdd) * (REGEN_MULT + regenMultAdd).</summary>
             public float regenMultAdd = 0f;
             /// <summary>Added to base health regen. HEALTH_REGEN ~ (BASE_REGEN + baseRegenAdd) * (REGEN_MULT + regenMultAdd).</summary>
             public float baseRegenAdd = 0f;
-            /// <summary>Added to the direct multiplier to move speed. MOVE_SPEED ~ BASE_MOVE_SPEED * (MOVE_SPEED_MULT + moveSpeedMultAdd)</summary>
+            /// <summary>Added to base move speed. MOVE_SPEED ~ (BASE_MOVE_SPEED + baseMoveSpeedAdd) * (MOVE_SPEED_MULT + moveSpeedMultAdd)</summary>
+            public float baseMoveSpeedAdd = 0f;
+            /// <summary>Added to the direct multiplier to move speed. MOVE_SPEED ~ (BASE_MOVE_SPEED + baseMoveSpeedAdd) * (MOVE_SPEED_MULT + moveSpeedMultAdd)</summary>
             public float moveSpeedMultAdd = 0f;
             /// <summary>Added to the direct multiplier to jump power. JUMP_POWER ~ BASE_JUMP_POWER * (JUMP_POWER_MULT + jumpPowerMultAdd)</summary>
             public float jumpPowerMultAdd = 0f;
@@ -36,7 +40,9 @@ namespace TILER2 {
             public float damageMultAdd = 0f;
             /// <summary>Added to base damage. DAMAGE ~ (BASE_DAMAGE + baseDamageAdd) * (DAMAGE_MULT + damageMultAdd).</summary>
             public float baseDamageAdd = 0f;
-            /// <summary>Added to the direct multiplier to attack speed. ATTACK_SPEED ~ BASE_ATTACK_SPEED * (ATTACK_SPEED_MULT + attackSpeedMultAdd).</summary>
+            /// <summary>Added to attack speed. ATTACK_SPEED ~ (BASE_ATTACK_SPEED + baseAttackSpeedAdd) * (ATTACK_SPEED_MULT + attackSpeedMultAdd).</summary>
+            public float baseAttackSpeedAdd = 0f;
+            /// <summary>Added to the direct multiplier to attack speed. ATTACK_SPEED ~ (BASE_ATTACK_SPEED + baseAttackSpeedAdd) * (ATTACK_SPEED_MULT + attackSpeedMultAdd).</summary>
             public float attackSpeedMultAdd = 0f;
             /// <summary>Added to crit chance. CRIT_CHANCE ~ BASE_CRIT_CHANCE + critAdd.</summary>
             public float critAdd = 0f;
@@ -56,6 +62,28 @@ namespace TILER2 {
         /// </summary>
         public static event StatHookEventHandler GetStatCoefficients;
 
+        /// <summary>
+        /// Matches the basic pattern of load base stat and level stat --> store combined as next stloc -->  read combined, then multiplier, and store result of multiplication.
+        /// </summary>
+        public static bool ILTryMatchBasicStatPattern(ILCursor c, string statName, ref int locBaseIndex, ref int locMultIndex) {
+            int _locBaseIndex = -1;
+            int _locMultIndex = -1;
+            bool ILFound = c.TryGotoNext(
+                x => x.MatchLdfld<CharacterBody>($"base{statName}"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>($"level{statName}"))
+                && c.TryGotoNext(
+                x => x.MatchStloc(out _locBaseIndex))
+                && c.TryGotoNext(
+                    x => x.MatchLdloc(_locBaseIndex),
+                    x => x.MatchLdloc(out _locMultIndex),
+                    x => x.MatchMul(),
+                    x => x.MatchStloc(_locBaseIndex));
+            locBaseIndex = _locBaseIndex;
+            locMultIndex = _locMultIndex;
+            return ILFound;
+        }
+
         //TODO: backup modifiers in an On. hook
         internal static void IL_CBRecalcStats(ILContext il) {
             ILCursor c = new ILCursor(il);
@@ -70,9 +98,9 @@ namespace TILER2 {
             int locBaseHealthIndex = -1;
             int locHealthMultIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x=>x.MatchLdfld<CharacterBody>("baseMaxHealth"),
-                x=>x.MatchLdarg(0),
-                x=>x.MatchLdfld<CharacterBody>("levelMaxHealth"))
+                x => x.MatchLdfld<CharacterBody>("baseMaxHealth"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>("levelMaxHealth"))
                 && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseHealthIndex))
                 && c.TryGotoNext(
@@ -94,7 +122,25 @@ namespace TILER2 {
             } else {
                 TILER2Plugin._logger.LogError("StatHooks: failed to apply IL patch (health modifier)");
             }
-            
+
+            c.Index = 0;
+
+            int locBaseShieldIndex = -1;
+            ILFound = c.TryGotoNext(
+                x => x.MatchLdfld<CharacterBody>("baseShield"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>("levelShield"))
+                && c.TryGotoNext(
+                    x => x.MatchStloc(out locBaseShieldIndex));
+
+            if(ILFound) {
+                c.EmitDelegate<Func<float, float>>((origBaseShield) => {
+                    return origBaseShield + statMods.baseShieldAdd;
+                });
+            } else {
+                TILER2Plugin._logger.LogError("StatHooks: failed to apply IL patch (shield modifier)");
+            }
+
             c.Index = 0;
 
             int locBaseRegenIndex = -1;
@@ -135,16 +181,30 @@ namespace TILER2 {
             
             c.Index = 0;
 
-            ILFound = c.TryGotoNext(MoveType.After,
-                x=>x.MatchLdfld<CharacterBody>("baseMoveSpeed"),
-                x=>x.MatchLdarg(0),
-                x=>x.MatchLdfld<CharacterBody>("levelMoveSpeed"),
-                x=>x.MatchLdloc(out _),
-                x=>x.MatchMul(),
-                x=>x.MatchAdd(),
-                x=>x.MatchStloc(out _),
-                x=>x.MatchLdcR4(out _));
+            int locBaseSpeedIndex = -1;
+            int locSpeedMultIndex = -1;
+            int locSpeedDivIndex = -1;
+            ILFound = c.TryGotoNext(
+                x => x.MatchLdfld<CharacterBody>("baseMoveSpeed"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>("levelMoveSpeed"))
+                && c.TryGotoNext(
+                x => x.MatchStloc(out locBaseSpeedIndex))
+                && c.TryGotoNext(
+                    x => x.MatchLdloc(locBaseSpeedIndex),
+                    x => x.MatchLdloc(out locSpeedMultIndex),
+                    x => x.MatchLdloc(out locSpeedDivIndex),
+                    x => x.MatchDiv(),
+                    x => x.MatchMul(),
+                    x => x.MatchStloc(locBaseSpeedIndex));
+
             if(ILFound) {
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("levelMoveSpeed"));
+                c.GotoNext(x => x.MatchStloc(locBaseSpeedIndex));
+                c.EmitDelegate<Func<float, float>>((origBaseMoveSpeed) => {
+                    return origBaseMoveSpeed + statMods.baseMoveSpeedAdd;
+                });
+                c.GotoNext(x => x.MatchStloc(locSpeedMultIndex));
                 c.EmitDelegate<Func<float, float>>((origMoveSpeedMult) => {
                     return origMoveSpeedMult + statMods.moveSpeedMultAdd;
                 });
@@ -154,7 +214,6 @@ namespace TILER2 {
             
             c.Index = 0;
 
-            //Find (parts of): float jumpPower = this.baseJumpPower + this.levelJumpPower * num32;
             ILFound = c.TryGotoNext(MoveType.After,
                 x=>x.MatchLdfld<CharacterBody>("baseJumpPower"),
                 x=>x.MatchLdarg(0),
@@ -173,22 +232,27 @@ namespace TILER2 {
             
             c.Index = 0;
 
-            ILFound = c.TryGotoNext(MoveType.After,
-                x=>x.MatchLdarg(0),
-                x=>x.MatchLdfld<CharacterBody>("baseDamage"),
-                x=>x.MatchLdarg(0),
-                x=>x.MatchLdfld<CharacterBody>("levelDamage"),
-                x=>x.MatchLdloc(out _),
-                x=>x.MatchMul(),
-                x=>x.MatchAdd(),
-                x=>x.MatchStloc(out _),
-                x=>x.MatchLdcR4(out _));
+            int locBaseDamageIndex = -1;
+            int locDamageMultIndex = -1;
+            ILFound = c.TryGotoNext(
+                x => x.MatchLdfld<CharacterBody>("baseDamage"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>("levelDamage"))
+                && c.TryGotoNext(
+                x => x.MatchStloc(out locBaseDamageIndex))
+                && c.TryGotoNext(
+                    x => x.MatchLdloc(locBaseDamageIndex),
+                    x => x.MatchLdloc(out locDamageMultIndex),
+                    x => x.MatchMul(),
+                    x => x.MatchStloc(locBaseDamageIndex));
+
             if(ILFound) {
-                c.Index -= 2;
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("baseDamage"));
+                c.GotoNext(x => x.MatchStloc(locBaseDamageIndex));
                 c.EmitDelegate<Func<float, float>>((origDamage) => {
                     return origDamage + statMods.baseDamageAdd;
                 });
-                c.Index += 2;
+                c.GotoNext(x => x.MatchStloc(locDamageMultIndex));
                 c.EmitDelegate<Func<float, float>>((origDamageMult) => {
                     return origDamageMult + statMods.damageMultAdd;
                 });
@@ -198,18 +262,29 @@ namespace TILER2 {
             
             c.Index = 0;
 
-            ILFound = c.TryGotoNext(MoveType.After,
-                x=>x.MatchLdfld<CharacterBody>("baseAttackSpeed"),
-                x=>x.MatchLdarg(0),
-                x=>x.MatchLdfld<CharacterBody>("levelAttackSpeed"),
-                x=>x.MatchLdloc(out _),
-                x=>x.MatchMul(),
-                x=>x.MatchAdd(),
-                x=>x.MatchStloc(out _),
-                x=>x.MatchLdcR4(out _));
+            int locBaseAttackSpeedIndex = -1;
+            int locAttackSpeedMultIndex = -1;
+            ILFound = c.TryGotoNext(
+                x => x.MatchLdfld<CharacterBody>("baseAttackSpeed"),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<CharacterBody>("levelAttackSpeed"))
+                && c.TryGotoNext(
+                x => x.MatchStloc(out locBaseAttackSpeedIndex))
+                && c.TryGotoNext(
+                    x => x.MatchLdloc(locBaseAttackSpeedIndex),
+                    x => x.MatchLdloc(out locAttackSpeedMultIndex),
+                    x => x.MatchMul(),
+                    x => x.MatchStloc(locBaseAttackSpeedIndex));
+
             if(ILFound) {
-                c.EmitDelegate<Func<float, float>>((origAttackSpeedMult) => {
-                    return origAttackSpeedMult + statMods.attackSpeedMultAdd;
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("baseAttackSpeed"));
+                c.GotoNext(x => x.MatchStloc(locBaseAttackSpeedIndex));
+                c.EmitDelegate<Func<float, float>>((origSpeed) => {
+                    return origSpeed + statMods.baseAttackSpeedAdd;
+                });
+                c.GotoNext(x => x.MatchStloc(locAttackSpeedMultIndex));
+                c.EmitDelegate<Func<float, float>>((origSpeedMult) => {
+                    return origSpeedMult + statMods.attackSpeedMultAdd;
                 });
             } else {
                 TILER2Plugin._logger.LogError("StatHooks: failed to apply IL patch (attack speed modifier)");
