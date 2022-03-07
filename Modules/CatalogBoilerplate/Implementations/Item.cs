9d293ce325aa07712f651a905e5d34d25537db73
ThinkInvisible <thinkinvis@gmail.com>
2022-03-07 14:32:19 -0600

    Switch off of obsoleted item color retrieval method

--- Modules/CatalogBoilerplate/Implementations/Item.cs
+++ Modules/CatalogBoilerplate/Implementations/Item.cs
@@ -47,10 +47,11 @@ namespace TILER2 {
                     if(runIsActive)
                         Run.instance.BuildDropTable();
                     if(args.oldValue != args.newValue) {
+                        var itemColor = ItemTierCatalog.GetItemTierDef(itemDef.tier).colorIndex;
                         if((bool)args.newValue == true) {
-                            if(Run.instance != null && Run.instance.enabled) Chat.AddMessage($"<color=#{ColorCatalog.GetColorHexString(itemDef.colorIndex)}>{displayName}</color> has been <color=#aaffaa>ENABLED</color>. It will now drop, and existing copies will start working again.");
+                            if(Run.instance != null && Run.instance.enabled) Chat.AddMessage($"<color=#{ColorCatalog.GetColorHexString(itemColor)}>{displayName}</color> has been <color=#aaffaa>ENABLED</color>. It will now drop, and existing copies will start working again.");
                         } else {
-                            if(Run.instance != null && Run.instance.enabled) Chat.AddMessage($"<color=#{ColorCatalog.GetColorHexString(itemDef.colorIndex)}>{displayName}</color> has been <color=#ffaaaa>DISABLED</color>. It will no longer drop, and existing copies will stop working.");
+                            if(Run.instance != null && Run.instance.enabled) Chat.AddMessage($"<color=#{ColorCatalog.GetColorHexString(itemColor)}>{displayName}</color> has been <color=#ffaaaa>DISABLED</color>. It will no longer drop, and existing copies will stop working.");
                         }
                     }
                 } else if(args.target.boundProperty.Name == nameof(itemIsAIBlacklisted)) {
