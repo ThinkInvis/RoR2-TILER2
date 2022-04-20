092f921e3bf878e739eb00f870805134e5fb8a47
ThinkInvisible <thinkinvis@gmail.com>
2022-04-20 11:59:42 -0500

    Make enigma/random-trigger configurable

--- Modules/CatalogBoilerplate/Implementations/Equipment.cs
+++ Modules/CatalogBoilerplate/Implementations/Equipment.cs
@@ -33,8 +33,15 @@ namespace TILER2 {
         [AutoConfigRoOSlider("{0:N0} s", 0f, 1000f)]
         [AutoConfig("The base cooldown of the equipment, in seconds.", AutoConfigFlags.DeferUntilNextStage, 0f, float.MaxValue)]
         public virtual float cooldown {get; protected set;} = 45f; //TODO: add a getter function to update ingame cooldown properly if in use; marked as DeferUntilNextStage until then
+
+        [AutoConfigRoOCheckbox()]
+        [AutoConfig("Whether the equipment can be granted by Artifact of Enigma.", AutoConfigFlags.DeferForever)]
         public virtual bool isEnigmaCompatible => true;
+
+        [AutoConfigRoOCheckbox()]
+        [AutoConfig("Whether the equipment can be triggered by Bottled Chaos.", AutoConfigFlags.DeferForever)]
         public virtual bool canBeRandomlyTriggered => true;
+
         public virtual bool isLunar => false;
 
         public override void SetupConfig() {
