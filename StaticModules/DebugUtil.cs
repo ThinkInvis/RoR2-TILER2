ed7ea17afa39c2d136d71bd4204726e40aee0505
ThinkInvisible <thinkinvis@gmail.com>
2022-04-30 09:48:40 -0500

    Add commands to spawn item models

--- StaticModules/DebugUtil.cs
+++ StaticModules/DebugUtil.cs
@@ -23,6 +23,98 @@ namespace TILER2 {
                 UnityEngine.SceneManagement.LoadSceneMode.Single);
         }
 
+        [ConCommand(commandName = "ir_sim", helpText = "Spawns an item's entire pickup model, for use with the item rendering scene and a runtime inspector.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by UnityEngine")]
+        private static void CCSpawnItemModel(ConCommandArgs args) {
+            var igh = GameObject.Find("ITEM GOES HERE (can offset from here)");
+            if(Run.instance || !igh) {
+                Debug.LogError("Cannot spawn an item model outside the item render scene (use concmd goto_itemrender).");
+                return;
+            }
+            if(args.Count < 1) {
+                TILER2Plugin._logger.LogError("ir_sim: missing argument 1 (item ID)!");
+                return;
+            }
+
+            ItemIndex item;
+            string itemSearch = args.TryGetArgString(0);
+            if(itemSearch == null) {
+                TILER2Plugin._logger.LogError("ir_sim: could not read argument 1 (item ID)!");
+                return;
+            } else if(int.TryParse(itemSearch, out int itemInd)) {
+                item = (ItemIndex)itemInd;
+                if(!ItemCatalog.IsIndexValid(item)) {
+                    TILER2Plugin._logger.LogError("ir_sim: argument 1 (item ID as integer ItemIndex) is out of range; no item with that ID exists!");
+                    return;
+                }
+            } else {
+                var results = ItemCatalog.allItems.Where((ind) => {
+                    var iNameToken = ItemCatalog.GetItemDef(ind).nameToken;
+                    var iName = Language.GetString(iNameToken);
+                    return iName.ToUpper().Contains(itemSearch.ToUpper());
+                });
+                if(results.Count() < 1) {
+                    TILER2Plugin._logger.LogError("ir_sim: argument 1 (item ID as string ItemName) not found in ItemCatalog; no item with a name containing that string exists!");
+                    return;
+                } else {
+                    if(results.Count() > 1)
+                        TILER2Plugin._logger.LogWarning("ir_sim: argument 1 (item ID as string ItemName) matched multiple items; using first.");
+                    item = results.First();
+                }
+            }
+
+            var idef = ItemCatalog.GetItemDef(item);
+            var idefModel = idef.pickupModelPrefab;
+
+            GameObject.Instantiate(idefModel, igh.transform);
+        }
+
+        [ConCommand(commandName = "ir_sqm", helpText = "Spawns an equipment's entire pickup model, for use with the item rendering scene and a runtime inspector.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by UnityEngine")]
+        private static void CCSpawnEquipmentModel(ConCommandArgs args) {
+            var igh = GameObject.Find("ITEM GOES HERE (can offset from here)");
+            if(Run.instance || !igh) {
+                Debug.LogError("Cannot spawn an equipment model outside the item render scene (use concmd goto_itemrender).");
+                return;
+            }
+            if(args.Count < 1) {
+                TILER2Plugin._logger.LogError("ir_sqm: missing argument 1 (item ID)!");
+                return;
+            }
+
+            EquipmentIndex item;
+            string itemSearch = args.TryGetArgString(0);
+            if(itemSearch == null) {
+                TILER2Plugin._logger.LogError("ir_sqm: could not read argument 1 (equipment ID)!");
+                return;
+            } else if(int.TryParse(itemSearch, out int itemInd)) {
+                item = (EquipmentIndex)itemInd;
+                if(!EquipmentCatalog.IsIndexValid(item)) {
+                    TILER2Plugin._logger.LogError("ir_sqm: argument 1 (equipment ID as integer EquipmentIndex) is out of range; no item with that ID exists!");
+                    return;
+                }
+            } else {
+                var results = EquipmentCatalog.allEquipment.Where((ind) => {
+                    var iNameToken = EquipmentCatalog.GetEquipmentDef(ind).nameToken;
+                    var iName = Language.GetString(iNameToken);
+                    return iName.ToUpper().Contains(itemSearch.ToUpper());
+                });
+                if(results.Count() < 1) {
+                    TILER2Plugin._logger.LogError("ir_sqm: argument 1 (equipment ID as string EquipmentName) not found in EquipmentCatalog; no equipment with a name containing that string exists!");
+                    return;
+                } else {
+                    if(results.Count() > 1)
+                        TILER2Plugin._logger.LogWarning("ir_sqm: argument 1 (equipment ID as string EquipmentName) matched multiple equipments; using first.");
+                    item = results.First();
+                }
+            }
+
+            var idef = EquipmentCatalog.GetEquipmentDef(item);
+            var idefModel = idef.pickupModelPrefab;
+
+            GameObject.Instantiate(idefModel, igh.transform);
+        }
+
         [ConCommand(commandName = "evo_setitem", flags = ConVarFlags.ExecuteOnServer
             #if !DEBUG
             | ConVarFlags.Cheat
