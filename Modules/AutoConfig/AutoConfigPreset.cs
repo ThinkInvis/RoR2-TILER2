71c75e7177e4d8c2124cb888db6a6820d112ae68
ThinkInvisible <thinkinvis@gmail.com>
2022-07-04 17:07:29 -0500

    Add implementation of ApplyPreset method

--- Modules/AutoConfig/AutoConfigPreset.cs
+++ Modules/AutoConfig/AutoConfigPreset.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace TILER2 {
     public static class AutoConfigPresetExtensions {
         public static void ApplyPreset(this AutoConfigContainer container, string name) {
+            HashSet<BepInEx.Configuration.ConfigFile> needsManualSave = new HashSet<BepInEx.Configuration.ConfigFile>();
 
+            foreach(var bind in container.bindings) {
+                var presets = bind.boundProperty.GetCustomAttributes(typeof(AutoConfigPresetAttribute), true).Cast<AutoConfigPresetAttribute>();
+                var matchingPreset = presets.FirstOrDefault(p => p.presetName == name);
+                if(matchingPreset != null) {
+                    bind.configEntry.BoxedValue = matchingPreset.boxedValue;
+                    if(!bind.configEntry.ConfigFile.SaveOnConfigSet)
+                        needsManualSave.Add(bind.configEntry.ConfigFile);
+                }
+            }
+
+            foreach(var sv in needsManualSave) {
+                sv.Save();
+            }
         }
     }
 
@@ -13,11 +27,11 @@ namespace TILER2 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
     public class AutoConfigPresetAttribute : Attribute {
         public readonly string presetName;
-        public readonly object value;
+        public readonly object boxedValue;
 
         public AutoConfigPresetAttribute(string name, object value) {
             this.presetName = name;
-            this.value = value;
+            this.boxedValue = value;
         }
     }
 }
